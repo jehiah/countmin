20a5045064740e9649c12af289aaecbeec57e2d3
Jehiah Czebotar <jehiah@gmail.com>
2012-10-14 14:06:08 -0400

    use a single contiguous array

--- sketch.go
+++ sketch.go
@@ -20,8 +20,8 @@ type Sketch interface {
 
 type countMinSketch struct {
 	Hashes  int
-	Columns uint32
-	Data    [][]uint32
+	Columns int
+	Data    []uint32
 }
 
 // Create a new Sketch. Settings for hashes and columns affect performance
@@ -29,11 +29,8 @@ type countMinSketch struct {
 func NewCountMinSketch(hashes int, columns int) Sketch {
 	s := countMinSketch{
 		Hashes:  hashes,
-		Columns: uint32(columns),
-		Data:    make([][]uint32, hashes),
-	}
-	for i, _ := range s.Data {
-		s.Data[i] = make([]uint32, columns)
+		Columns: columns,
+		Data:    make([]uint32, hashes * columns),
 	}
 	return &s
 }
@@ -50,12 +47,13 @@ func (s *countMinSketch) Add(key []byte, count uint32) uint32 {
 	newValue := s.Query(key) + count
 	h := fnv.New64a()
 	h.Write(key)
+	columns := uint32(s.Columns)
 	var b []byte
-	for i := 0; i < s.Hashes; i += 1 {
-		binary.Write(h, binary.LittleEndian, uint32(i))
-		index := crc32.ChecksumIEEE(h.Sum(b)) % s.Columns
-		if s.Data[i][index] <= newValue {
-			s.Data[i][index] = newValue
+	for base := uint32(0); base < uint32(s.Hashes) * columns; base += columns {
+		binary.Write(h, binary.LittleEndian, uint32(base))
+		index := crc32.ChecksumIEEE(h.Sum(b)) % columns
+		if s.Data[base + index] <= newValue {
+			s.Data[base + index] = newValue
 		}
 	}
 	return newValue
@@ -66,11 +64,12 @@ func (s *countMinSketch) Query(key []byte) uint32 {
 	h.Write(key)
 	var min uint32
 	var b []byte
-	for i := 0; i < s.Hashes; i += 1 {
-		binary.Write(h, binary.LittleEndian, uint32(i))
-		index := crc32.ChecksumIEEE(h.Sum(b)) % s.Columns
-		v := s.Data[i][index]
-		if i == 0 || v < min {
+	columns := uint32(s.Columns)
+	for base := uint32(0); base < uint32(s.Hashes) * columns; base += columns {
+		binary.Write(h, binary.LittleEndian, uint32(base))
+		index := crc32.ChecksumIEEE(h.Sum(b)) % columns
+		v := s.Data[base + index]
+		if base == 0 || v < min {
 			min = v
 		}
 	}
